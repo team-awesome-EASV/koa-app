80582b79d30ecafccb9194817cff1df182eb9c7f
julia <julia.gru@gmail.com>
2020-09-22 09:54:14 +0200

    minor thingy

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -13,7 +13,7 @@
         v-model="name"
         label="Name of workshop"
         hint="enter the name of the workshop"
-        lazy-rules
+        :lazy-rules="true"
         :rules="[ val => val && val.length > 0 || 'Please type something']"
       />
       <q-input
@@ -22,7 +22,7 @@
         v-model="textDescription"
         label="Large description of Workshop"
         hint="this descrition wll be used to display information about the workshop on the webpage as a large paragraph"
-        lazy-rules
+        :lazy-rules="true"
         :rules="[ val => val && val.length > 0 || 'Please type something']"
       />
 
@@ -32,7 +32,7 @@
         v-model="modulesNo"
         label="Number of modules"
         hint="enter the number of modules the worshop has"
-        lazy-rules
+        :lazy-rules="true"
         :rules="[
           val => val !== null && val !== '' || 'Please type your age',
           val => val > 0 && val < 100 || 'Please type a real age'
@@ -45,7 +45,7 @@
         v-model="duration"
         label="Number of weeks"
         hint="enter the duration of the workshop in weeks"
-        lazy-rules
+        :lazy-rules="true"
         :rules="[
           val => val !== null && val !== '' || 'Please type your age',
           val => val > 0 && val < 100 || 'Please type a real age'
@@ -56,7 +56,7 @@
         v-model="keyWords"
         label="Key Words"
         hint="Name up to 10 key words that best describe the Workshop, use comma and space ', ' to seaprate them"
-        lazy-rules
+        :lazy-rules="true"
         :rules="[ val => val && val.length > 0 || 'Please type something']"
       />
 
