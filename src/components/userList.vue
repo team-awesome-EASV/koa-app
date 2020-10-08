6fe9b8a6dd713e1f6adbd624412d526d180e36e8
darthradek <radoslawhaller@gmail.com>
2020-10-08 17:38:44 +0200

    ccomit

--- src/components/userList.vue
+++ src/components/userList.vue
@@ -1,29 +1,105 @@
 <template>
-<div class="full-width row wrap justify-start items-start content-start text-primary">
+<div>
+    <q-card class="q-ma-lg col-12 col-md-6" flat>
+        <div class="q-pa-md">
+            <q-toolbar class="bg-primary text-white shadow-2">
+            <q-toolbar-title>Contacts</q-toolbar-title>
+            </q-toolbar>
 
+                <q-list>
+                <q-item v-for="contact in contacts" :key="contact.id" class="q-my-sm" clickable v-ripple>
+                <q-item-section avatar>
+                <q-avatar color="primary" text-color="white">
+                    {{ contact.letter }}
+                </q-avatar>
+                </q-item-section>
 
+                <q-item-section class="q-ma-md">
+                <q-item-label>{{ contact.name }}</q-item-label>
+                <q-item-label caption lines="1">{{ contact.email }}</q-item-label>
+                </q-item-section>
+
+                <q-separator vertical inset />
+
+                <q-item-section class="q-ma-md">
+                <q-item-label>Kids</q-item-label>
+                <q-item-label caption lines="1">{{ contact.kids }}</q-item-label>
+                </q-item-section>
+
+                <q-item-section side class="q-ma-sm">
+                <q-icon name="more" color="primary" />
+                </q-item-section>
+
+            </q-item>
+            </q-list>
+        </div>
+    </q-card>
 </div>
 </template>
 
 <script>
 import { mapGetters,mapActions } from "vuex";
 
-export default {
-    data() {
-        return {
-           
-        };
-    },
+const contacts = [ {
+  id: 1,
+  name: 'Ruddy Jedrzej',
+  email: 'rjedrzej0@discuz.net',
+  letter: 'R',
+  kids: 'Camil, Anne'
+}, {
+  id: 2,
+  name: 'Mallorie Alessandrini',
+  email: 'malessandrini1@marketwatch.com',
+  letter: 'M',
+  kids: 'Adio, Ezio'
+}, {
+  id: 3,
+  name: 'Elisabetta Wicklen',
+  email: 'ewicklen2@microsoft.com',
+  letter: 'E',
+  kids: 'Mariusz, Eryk, Krzysztof'
+}, {
+  id: 4,
+  name: 'Seka Fawdrey',
+  email: 'sfawdrey3@wired.com',
+  letter: 'S',
+  kids: 'Enio, Julia'
+}, {
+  id: 4,
+  name: 'Maciek Matus',
+  email: 'matusek982@wired.com',
+  letter: 'M',
+  kids: 'Camilla'
+}, {
+  id: 4,
+  name: 'Elena Ckicka',
+  email: 'sznyblas23@wired.com',
+  letter: 'E',
+  kids: 'Natalia'
+}, {
+  id: 4,
+  name: 'Ziger Matuś',
+  email: 'mate2@wired.com',
+  letter: 'M',
+  kids: 'Andrew'
+}, {
+  id: 4,
+  name: 'Jarek Kiełbaska',
+  email: 'JarekK@pępek.com',
+  letter: 'J',
+  kids: 'Ellen'
+}  ]
 
-    computed: {
-      
-    },
-    methods: {
-         
+
+export default {
+  data () {
+    return {
+      contacts,
+       lorem: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+   
     }
-};
+  }
+}
 </script>
 
-<style lang="css">
 
-</style>
--- src/pages/Users.vue
+++ src/pages/Users.vue
@@ -1,24 +1,22 @@
 <template>
-  <div class=" text-white text-center q-pa-md flex flex-center">
-    <div>
-      <div style="color:pink; font-size: 20vh">
-        Users Page
-      </div>
-      <q-btn
-        class="q-mt-xl"
-        color="white"
-        text-color="blue"
-        unelevated
-        to="/"
-        label="Go Home"
-        no-caps
-      />
-    </div>
+  <div class="full-width row wrap justify-center items-start content-start text-primary">
+      <q-card class="q-ma-lg col-12 col-md-6">
+        <userList></userList>
+      </q-card>
+      <q-card class="my-card q-ma-lg col-12 col-md-5">
+        <userDetails></userDetails>
+      </q-card>
   </div>
 </template>
 
 <script>
+import userList from "../components/userList.vue"
+import userDetails from "../components/users/userDetails.vue"
+
 export default {
-  name: 'Users'
+  components:{
+    userList: userList,
+    userDetails: userDetails
+  }
 }
 </script>
