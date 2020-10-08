a44625aac9229a5653aa2d7176d2db524c84db11
julia <julia.gru@gmail.com>
2020-10-08 17:36:01 +0200

    step one ui in progress

--- src/store/workshops/index.js
+++ src/store/workshops/index.js
@@ -1,39 +1,50 @@
-import { workshop } from 'src/boot/firebase.js'
+import { workshop } from "src/boot/firebase.js";
 
 export default {
   namespaced: true,
 
   state: {
     allWorkshops: [],
-    activeWorkshop: {
-    }
+    activeWorkshop: {}
   },
 
   getters: {
     allWorkshops: state => state.allWorkshops,
     activeWorkshop: state => state.activeWorkshop,
+    workshopsSelect: state =>
+      state.allWorkshops.map(el => ({ label: el.name, value: el.id })),
+    moduleSelect: state => id => {
+      return state.allWorkshops.find(workshop => workshop.id === id);
+    }
   },
 
   actions: {
-
-    addNewWorkshopToDatabase ( { commit }, payload ) {
+    addNewWorkshopToDatabase({ commit }, payload) {
       var content = {};
-      workshop.add( {
-        ...payload
-      } )
-        .then( workshopElement => {
-          workshop.doc( workshopElement.id ).update( {
-            workshopId: workshopElement.id,
-            workshopPath: workshopElement.path,
-          } )
-            .then( () => {
-              workshop.doc( workshopElement.id ).onSnapshot( workshopElementTwo => {
-                console.log( "display value of worshop after update", workshopElementTwo.data() );
-                this.content = workshopElementTwo.data();
-                console.log( "this is the content", this.content );
-                commit( "setActiveWorkshop", this.content );
-              } )
-            } )
+      workshop
+        .add({
+          ...payload
+        })
+        .then(workshopElement => {
+          workshop
+            .doc(workshopElement.id)
+            .update({
+              workshopId: workshopElement.id,
+              workshopPath: workshopElement.path
+            })
+            .then(() => {
+              workshop
+                .doc(workshopElement.id)
+                .onSnapshot(workshopElementTwo => {
+                  console.log(
+                    "display value of worshop after update",
+                    workshopElementTwo.data()
+                  );
+                  this.content = workshopElementTwo.data();
+                  console.log("this is the content", this.content);
+                  commit("setActiveWorkshop", this.content);
+                });
+            });
           // console.log( "Document successfully written!", workshopElement )
           // .then( () => {
           //   if ( content ) {
@@ -42,74 +53,79 @@ export default {
           //   }
 
           // } )
+        })
 
-        } )
-
-        .catch( function ( error ) {
-          console.error( "Error writing document: ", error );
-        } )
-
+        .catch(function(error) {
+          console.error("Error writing document: ", error);
+        });
     },
 
-    addNewModuleToWorkshop ( { }, { info, id } ) {
-      console.log( " active workshop with this", info );
-      console.log( " active workshop with this", id );
-      workshop.doc( id ).collection( 'Modules' ).add( {
-        ...info
-      } )
-        .then( moduleElement => {
-          workshop.doc( id ).collection( 'Modules' ).doc( moduleElement.id ).update( {
-            workshopId: moduleElement.id,
-            workshopPath: moduleElement.path,
-          } )
-        } )
-        .catch( error => {
-          console.error( "Error writing document: ", error );
-        } )
+    addNewModuleToWorkshop({}, { info, id }) {
+      console.log(" active workshop with this", info);
+      console.log(" active workshop with this", id);
+      workshop
+        .doc(id)
+        .collection("Modules")
+        .add({
+          ...info
+        })
+        .then(moduleElement => {
+          workshop
+            .doc(id)
+            .collection("Modules")
+            .doc(moduleElement.id)
+            .update({
+              workshopId: moduleElement.id,
+              workshopPath: moduleElement.path
+            });
+        })
+        .catch(error => {
+          console.error("Error writing document: ", error);
+        });
     },
 
-
-    async setWorkshops ( state ) {
+    async setWorkshops(state) {
       var workshopList = [];
       var content = null;
-      workshop.onSnapshot( workshopItems => {
+      workshop.onSnapshot(workshopItems => {
         workshopList = [];
-        workshopItems.forEach( doc => {
-
-          console.log( "this is the workshop", workshopItems )
+        workshopItems.forEach(doc => {
+          console.log("this is the workshop", workshopItems);
           var workShopData = doc.data();
-          workshopList.push( {
+          workshopList.push({
             ...workShopData,
             id: doc.id
-          } );
-        } );
+          });
+        });
         content = workshopList;
-        state.commit( "setAllWorkshops", content );
-      } );
+        state.commit("setAllWorkshops", content);
+      });
     },
 
-    deleteWorkshopFromDatabase ( { }, payload ) {
-      workshop.doc( payload ).delete().then( () => {
-        console.log( "deleted item with ID", payload );
-      } ).catch( error => {
-        console.log( "there was an error", error.message )
-      } )
+    deleteWorkshopFromDatabase({}, payload) {
+      workshop
+        .doc(payload)
+        .delete()
+        .then(() => {
+          console.log("deleted item with ID", payload);
+        })
+        .catch(error => {
+          console.log("there was an error", error.message);
+        });
     }
   },
 
   mutations: {
-    setAllWorkshops ( state, content ) {
-
+    setAllWorkshops(state, content) {
       state.allWorkshops = [];
-      console.log( "this should be empty", state.allWorkshops );
+      console.log("this should be empty", state.allWorkshops);
       state.allWorkshops = content;
-      console.log( "THIS IS Workshop", state.allWorkshops );
+      console.log("THIS IS Workshop", state.allWorkshops);
     },
 
-    setActiveWorkshop ( state, content ) {
-      console.log( "this is the content of activeworkshop mutation", content )
+    setActiveWorkshop(state, content) {
+      console.log("this is the content of activeworkshop mutation", content);
       state.activeWorkshop = content;
     }
-
   }
 };
