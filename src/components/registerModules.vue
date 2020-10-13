4e4e064e4ba1fc5a1e8784ae9043361c9059c4e2
Raul-Octavina Albu <alburaul@gmil.com>
2020-10-13 16:56:53 +0200

    stepper implemented

--- src/components/registerModules.vue
+++ src/components/registerModules.vue
@@ -64,7 +64,7 @@
           />
 
           <q-editor
-            ref="editor_ref"
+            ref="editor_ref_module"
             @paste.native="evt => pasteCapture(evt)"
             v-model="moduleInfo.moduleDescription"
             toolbar-text-color="white"
@@ -216,14 +216,14 @@ export default {
       evt.preventDefault();
       if (evt.originalEvent && evt.originalEvent.clipboardData.getData) {
         text = evt.originalEvent.clipboardData.getData("text/plain");
-        this.$refs.editor_ref.runCmd("insertText", text);
+        this.$refs.editor_ref_module.runCmd("insertText", text);
       } else if (evt.clipboardData && evt.clipboardData.getData) {
         text = evt.clipboardData.getData("text/plain");
-        this.$refs.editor_ref.runCmd("insertText", text);
+        this.$refs.editor_ref_module.runCmd("insertText", text);
       } else if (window.clipboardData && window.clipboardData.getData) {
         if (!onPasteStripFormattingIEPaste) {
           onPasteStripFormattingIEPaste = true;
-          this.$refs.editor_ref.runCmd("ms-pasteTextOnly", text);
+          this.$refs.editor_ref_module.runCmd("ms-pasteTextOnly", text);
         }
         onPasteStripFormattingIEPaste = false;
       }
