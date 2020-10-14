47467c78ac02f83e19cadb517d354ad2a408fb7e
julia <julia.gru@gmail.com>
2020-10-14 17:53:00 +0200

    syntax error fix

--- src/store/users/index.js
+++ src/store/users/index.js
@@ -1,46 +1,44 @@
 import { users } from "src/boot/firebase.js";
 
-
 export default {
-    namespaced: true,  
+  namespaced: true,
 
   state: {
-    allUsers: [],
+    allUsers: []
   },
 
-  mutations:{
-
+  mutations: {
     committAllUsers: (state, payload) => {
-        console.log("is this even happening?", payload);
-        state.allUsers = payload;
-      },
-
-  },
-
-  actions:{
-
-    getAllUsers: context => {
-        let allUsersTemp = []
-
-        users.onSnapshot((snapshotItems) => {
-       
-            snapshotItems.forEach((doc) => {
-                let userDetailsData = doc.data();
-                allUsersTemp.push({
-                    id: doc.id,
-                    name: userDetailsData.name,
-                    email: userDetailsData.email,
-                    initial: userDetailsData.name.split(' ').map(word => word.charAt(0)).join('')
-                });
-            });
-            allUsersTemp = [];
-        }).then(() => {context.commit("committAllUsers", allUsersTemp);
+      console.log("is this even happening?", payload);
+      state.allUsers = payload;
+    }
   },
 
   getters: {
     userDetails: state => state.allUsers
+  },
+
+  actions: {
+    getAllUsers: context => {
+      let allUsersTemp = [];
+
+      users.onSnapshot(snapshotItems => {
+        snapshotItems.forEach(doc => {
+          let userDetailsData = doc.data();
+          allUsersTemp.push({
+            id: doc.id,
+            name: userDetailsData.name,
+            email: userDetailsData.email,
+            initial: userDetailsData.name
+              .split(" ")
+              .map(word => word.toUpperCase().charAt(0))
+              .join("")
+          });
+        });
+        allUsersTemp = [];
+      });
+
+      context.commit("committAllUsers", allUsersTemp);
+    }
   }
-
-
-  
 };
